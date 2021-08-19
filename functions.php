5bd9bcb03b44ddeb5f0e2ac730448091d95533dc
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2021-08-19 23:37:43 +0900

    add calcProcessTime

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+
+function calcProcessTime() {
+	// ログインしているユーザーにのみ実行されるよう判定
+	if ( is_user_logged_in() ) {
+		// 開始時刻の取得
+		$start = microtime( true );
+		
+		// 値が0の変数を用意
+		$num = 0;
+		
+		// +1を10億回繰り返し
+		for ( $i = 0; $i < 1000000000; $i++ ){
+			$num++;
+		}
+		
+		// 開始と現在の時間差（＝処理にかかった時間）を求める
+		$time = ( microtime( true ) - $start );
+		
+		// console.logに結果を出力
+		echo '<script>console.log(' . json_encode( round( $time, 3 ) ) . ');</script>';
+	}
+}
+
+calcProcessTime();
